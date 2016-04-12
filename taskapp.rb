a0d9e974ff1d52ad46f26ac837a9b6c1a30eda00
Sarah Nahmias <nahmisa@gmail.com>
2016-04-12 16:07:10 -0700

    Made the task lookup a string so the method works

--- taskapp.rb
+++ taskapp.rb
@@ -14,7 +14,7 @@ class MyApp < Sinatra::Base
   end
 
   post '/new-task' do
-    @tasks = params[tasks]
+    @tasks = params["tasks"]
     @task_list_database = TaskList::Task.new
     @task_list_database.insert!(@tasks)
 
